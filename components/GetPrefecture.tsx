cad1c424973765e78a8c2d9976ac57ff63bced6a
jumpei-jp <junpeiyamaoka0516@gmail.com>
2022-06-18 21:29:01 +0900

    feat: チェックボックスにチェック入ると対象地域情報が取得できる

--- components/GetPrefecture.tsx
+++ components/GetPrefecture.tsx
@@ -2,9 +2,18 @@ import { useState, useEffect } from 'react'
 
 export default function Prefecture() {
     const [prefectures, setPrefecture] = useState<Pref[]>([])
+    const [populations, setPopulation] = useState<Population[]>([])
     type Pref = {
         prefCode: number
         prefName: string
+    }
+    type Population = {
+        prefCode: number
+        prefName: string
+        data: {
+            year: number
+            value: number
+        }[]
     }
     //都道府県情報を取得
     useEffect(() => {
@@ -22,9 +31,42 @@ export default function Prefecture() {
         prefCode: number,
         isChecked: boolean
     ) => {
-        console.log(prefCode + ':' + prefName + 'に変化がありました。')
-        console.log(isChecked)
+        getPopulation(prefName, prefCode)
     }
+
+    //人口を取得する
+    const getPopulation = async (prefName: string, prefCode: number) => {
+        const fetchPopulation = async () => {
+            const response = await fetch('/api/populations/population')
+            const result = await response.json()
+
+            //人口とそれに対応する都道府県情報を追加する
+            let prefPopulation = populations.slice()
+            const populationData: PopulationData = {
+                prefName: prefName,
+                prefCode: prefCode,
+                data: result.data,
+            }
+
+            prefPopulation.push({
+                prefName: prefName,
+                prefCode: prefCode,
+                data: result,
+            })
+            setPopulation(prefPopulation)
+        }
+
+        fetchPopulation()
+    }
+    type PopulationData = {
+        prefName: string
+        prefCode: number
+        data: {
+            year: number
+            value: number
+        }[]
+    }
+
     return (
         <>
             {prefectures.map((pref: Pref, index) => (
