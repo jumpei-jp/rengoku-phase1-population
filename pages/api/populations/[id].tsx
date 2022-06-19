ac6d5d75b6110508ac65587c8d7720494f101f78
jumpei-jp <junpeiyamaoka0516@gmail.com>
2022-06-19 12:18:49 +0900

    fix: APIキーを直入力していたところを環境変数から呼び出すようにした

--- pages/api/populations/[id].tsx
+++ pages/api/populations/[id].tsx
@@ -1,15 +1,18 @@
 const Population = async (req: any, res: any) => {
     //人口を取得
-    // TODO: 環境変数を呼び出すようにする
-    const response = await fetch(
-        `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${req.query.id}`,
-        {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': '3U4qwUJkm2IuBHPxO4wc3MQ7aocqNCF0kjCSVVhR',
-            },
+    const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?&prefCode=${req.query.id}`
+    const params: Params = {
+        method: 'GET',
+        headers: { 'X-API-KEY': process.env.OPEN_POPULATION_API_KEY },
+    }
+    const response = await fetch(url, params)
+
+    type Params = {
+        method: string
+        headers: {
+            'X-API-KEY': string
         }
-    )
+    }
 
     //APIの結果を返す
     const data = await response.json()
--- pages/api/prefectures/prefecture.tsx
+++ pages/api/prefectures/prefecture.tsx
@@ -1,21 +1,19 @@
 const Prefecture = async (req: any, res: any) => {
-    type Response = {
-        method: 'GET'
+    // 都道府県名、都道府県コードを取得
+    const url = `https://opendata.resas-portal.go.jp/api/v1/prefectures`
+    const params: Params = {
+        method: 'GET',
+        headers: { 'X-API-KEY': process.env.OPEN_POPULATION_API_KEY },
+    }
+    const response = await fetch(url, params)
+
+    type Params = {
+        method: string
         headers: {
             'X-API-KEY': string
         }
     }
 
-    //都道府県名、都道府県コードを取得
-    const response = await fetch(
-        `https://opendata.resas-portal.go.jp/api/v1/prefectures`,
-        {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': '3U4qwUJkm2IuBHPxO4wc3MQ7aocqNCF0kjCSVVhR',
-            },
-        }
-    )
     //apiを叩いた結果をdataに入れてステータスコード200でjsonで返す
     const data = await response.json()
     res.status(200).json(data)
